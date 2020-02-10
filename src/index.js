504da9b98da9f877bf529ad3e364c3a9e36adc34
Blank <blank@localhost.localdomain>
2020-02-10 20:45:38 +0545

    change path to run in github

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './app.css';
+import 'index.css';
+import 'app.css';
 import './components/styles/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
