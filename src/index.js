efe0beb31a997d6f31d046894b246ca442788f1f
Blank <blank@localhost.localdomain>
2020-02-10 21:43:37 +0545

    little changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'index.css';
-import 'app.css';
+import './index.css';
 import './components/styles/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
