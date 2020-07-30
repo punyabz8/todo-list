ecb5253f43d781eb1282ca4f191bc117d3157520
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-30 03:31:04 +0000

    Deploy susan888/todo-list to github.com/susan888/todo-list.git:gh-pages

---
+++ precache-manifest.67a74156edd63ba18c9d8c30dd630a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910613480f5ac446c7fb3d5e7032e27a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "6deaf88b2436523043de",
+    "url": "/todo-list/static/css/main.fe0a1f65.chunk.css"
+  },
+  {
+    "revision": "94d141b419a9f3936e70",
+    "url": "/todo-list/static/js/2.4b7e2b6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-list/static/js/2.4b7e2b6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6deaf88b2436523043de",
+    "url": "/todo-list/static/js/main.8af537b0.chunk.js"
+  },
+  {
+    "revision": "bb908536ea7e683d1530",
+    "url": "/todo-list/static/js/runtime-main.ad798cf3.js"
+  }
+]);
